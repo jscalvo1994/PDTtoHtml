7bbbd3d54d06a0bd73851eeeaaec6fea5f5361a4
jscalvo1994 <juansebastian.calvo408@comunidadunir.net>
2025-07-11 20:40:08 -0500

    Importadores directos añadidos

---
+++ src/app/page.jsx
@@ -0,0 +1,45 @@
+import Hero from "@/components/Hero/Hero.jsx";
+import IndexSection from "@/components/Index/IndexSection.jsx";
+import SocialMedia from "@/components/Index/SocialMedia.jsx";
+import GeneralIndex from "@/components/Index/GeneralIndex.jsx";
+import BisagrasContent from "@/components/Bisagras/BisagrasContent.jsx";
+import BisagrasContent2 from "@/components/Bisagras/BisagrasContent2.jsx";
+import GaleriaBisagra from "@/components/Galerias/GaleriaBisagras.jsx";
+import BisagrasContent3 from "@/components/Bisagras/BisagrasContent3.jsx";
+import BisagrasContent4 from "@/components/Bisagras/BisagrasContent4.jsx";
+import BisagrasContent5 from "@/components/Bisagras/BisagrasContent5.jsx";
+import BisagrasContent6 from "@/components/Bisagras/BisagrasContent6.jsx";
+import BisagrasContent7 from "@/components/Bisagras/BisagrasContent7.jsx";
+import BisagrasContent8 from "@/components/Bisagras/BisagrasContent8.jsx";
+import GaleriaManijas from "@/components/Galerias/GaleriaManijas.jsx";
+import ManijasComponent9 from "@/components/Manijas/ManijasComponent9.jsx";
+import ManijasComponent10 from "@/components/Manijas/ManijasComponent10.jsx";
+import ManijasComponent11 from "@/components/Manijas/ManijasComponent11.jsx";
+import GaleriaImport from "@/components/Galerias/GaleriaImport.jsx";
+
+export default function Home() {
+  return (
+    <>
+      <div>
+        <Hero />
+        <SocialMedia />
+        <IndexSection />
+        <GeneralIndex />
+        <BisagrasContent />
+        <BisagrasContent2 />
+        <GaleriaBisagra />
+        <BisagrasContent3 />
+        <BisagrasContent4 />
+        <BisagrasContent5 />
+        <BisagrasContent6 />
+        <BisagrasContent7 />
+        <BisagrasContent8 />
+        <GaleriaManijas />
+        <ManijasComponent9 />
+        <ManijasComponent10 />
+        <ManijasComponent11 />
+        <GaleriaImport />
+      </div>
+    </>
+  );
+}
